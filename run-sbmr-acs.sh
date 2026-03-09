1a947b05c275c71328c29f986580d962ec8e84cc
gurrev01 <gururaj.revankar@arm.com>
2026-03-09 07:33:39 +0000

    Auto-discover Redfish IDs from config BMC credentials

--- run-sbmr-acs.sh
+++ run-sbmr-acs.sh
@@ -20,6 +20,43 @@ CONSOLE_LOG=$OUTPUT_DIR/console.log
 DEBUG=""
 INTERFACE=""
 
+# Auto-discover Redfish IDs if BMC creds are in config
+function discover_redfish_ids() {
+    function get_config_value() {
+        local key="${1}"
+
+        awk -F: -v k="${key}" '$0 ~ "^-v "k":" {print $2; exit}' config
+    }
+
+    function get_redfish_id() {
+        curl -sk --max-time 10 --retry 2 --retry-delay 1 -u "$2:$3" \
+            "https://$1/redfish/v1/$4" \
+            | grep -o "/redfish/v1/$4/[^\"]*" | head -n1 | awk -F/ '{print $NF}'
+    }
+
+    local bmc_host bmc_user bmc_pass discover_failed=0 item coll key value
+    bmc_host=$(get_config_value "BMC_HOST")
+    bmc_user=$(get_config_value "BMC_USERNAME")
+    bmc_pass=$(get_config_value "BMC_PASSWORD")
+    if [ -n "${bmc_host}" ] && [ -n "${bmc_user}" ] && [ -n "${bmc_pass}" ] ; then
+        for item in "Managers:BMC_ID" "Systems:SYSTEM_ID" "Chassis:CHASSIS_ID" ; do
+            coll="${item%%:*}"
+            key="${item##*:}"
+            value=$(get_redfish_id "${bmc_host}" "${bmc_user}" "${bmc_pass}" "${coll}")
+            if [ -n "${value}" ] && grep -q "^-v ${key}:" config ; then
+                sed -i "s/^-v ${key}:.*/-v ${key}:${value}/" config
+            else
+                [ -z "${value}" ] && discover_failed=1
+            fi
+        done
+    else
+        discover_failed=1
+    fi
+    if [ "${discover_failed}" -eq 1 ] ; then
+        echo -e "Warning: Add correct BMC information in config file to run SBMR-ACS out-of-band test.\n"
+    fi
+}
+
 # Save config file in logs
 if [ ! -d "$OUTPUT_DIR" ]; then
   mkdir logs
@@ -48,6 +85,7 @@ done
 echo ""
 case "$INTERFACE" in
   'oob')
+    discover_redfish_ids
     echo "===== Running sbmr-acs-oob test suite ====="
     robot --argumentfile config --argumentfile test_lists/sbmr-acs-oob $DEBUG \
       --name "SBMR-ACS OOB" . | tee $CONSOLE_LOG
@@ -62,4 +100,3 @@ case "$INTERFACE" in
     echo -e "Example:\n\t ./run-sbmr-acs oob\n\t ./run-sbmr-acs oob -d\n\t"
     ;;
 esac
-
